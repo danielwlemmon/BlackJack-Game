bedd7ff8d892596dc903295d23712c0fc412d805
Daniel <dlemmonsqueeze@gmail.com>
2021-12-09 18:54:40 -0700

    Lots of updates to adding the images of cards instead of the text to represent the hands dealt.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const SUITS = ["♠", "♣", "♥", "♦"]
-const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
-let gameDeck = [];
-
-
+// global vairables
+//const beginGame = document.getElementById("beginGame");
+var gameDeck = [];
 var btnNew = document.getElementById("beginGame");
+
 btnNew.addEventListener("click", beginGame);
 
 class Player {
@@ -21,13 +19,13 @@ let player = new Player("player", [], 0, 0, 100);
 let dealer = new Player("dealer", [], 0, 0, 1000);
 
 function buildDeck() {
-  let deck = []; //use different method rather than two for loops
-  for (let s = 0; s < SUITS.length; s++) {
-    for (let v = 0; v < VALUES.length; v++) {
-      deck.push([VALUES[v], SUITS[s]]);
-    }
-  }
-  return deck;
+  gameDeck = [
+    ["c2", 2], ["c3", 3], ["c4", 4], ["c5", 5], ["c6", 6], ["c7", 7], ["c8", 8], ["c9", 9], ["c10", 10], ["cj", 10], ["cq", 10], ["ck", 10], ["ca", 11],
+    ["d2", 2], ["d3", 3], ["d4", 4], ["d5", 5], ["d6", 6], ["d7", 7], ["d8", 8], ["d9", 9], ["d10", 10], ["dj", 10], ["dq", 10], ["dk", 10], ["da", 11],
+    ["h2", 2], ["h3", 3], ["h4", 4], ["h5", 5], ["h6", 6], ["h7", 7], ["h8", 8], ["h9", 9], ["h10", 10], ["hj", 10], ["hq", 10], ["hk", 10], ["ha", 11],
+    ["s2", 2], ["s3", 3], ["s4", 4], ["s5", 5], ["s6", 6], ["s7", 7], ["s8", 8], ["s9", 9], ["s10", 10], ["sj", 10], ["sq", 10], ["sk", 10], ["sa", 11]
+  ];
+  return gameDeck;
 }
 
 function shuffleDeck(deck) {
@@ -38,7 +36,6 @@ function shuffleDeck(deck) {
     deck[newIndex] = deck[i];
     deck[i] = oldValue;
   }
-
   return deck;
 }
 
@@ -52,6 +49,12 @@ function resetGame() {
   player.currentBet = 10;
   player.money -= player.currentBet;
   gameDeck = shuffleDeck(buildDeck());
+  //remove image sources.
+  for (let i = 0; i < 7; i++) {
+    document.getElementById('dealerCard' + (i + 1)).src = "";
+    document.getElementById('playerCard' + (i + 1)).src = "";
+  }
+
   //deal initial cards alternating player/dealer.  Make this a function when adding more players.
   oneToPlayer();
   oneToDealer();
@@ -67,16 +70,12 @@ function calcHand(hand) {
   for (let i = 0; i < hand.length; i++) {
     const currentCard = hand[i];
 
-    if (currentCard[0] == "A") {
+    if (currentCard[1] == 11) {
       aceCount += 1;
       total += 11;
-
-    } else if (currentCard[0] == "K" || currentCard[0] == "Q" || currentCard[0] == "J" || currentCard[0] == "10") {
-      total += 10;
-
     } else {
-      total += parseInt(currentCard[0]);
-    }
+      total += (currentCard[1]);
+    };
   }
   if (aceCount > 0 && total > 21) {
     total = total - 10;
@@ -85,18 +84,41 @@ function calcHand(hand) {
     }
   }
   return total;
-}
+};
 
 function oneToPlayer() {
+
+  //const test = $(`#playerCard ${imageSlot}`).src = `/cardImg/ ${currentCard[0]}.png`;
+  //console.log(test)
+
+  //take card from deck and give it to player
   player.hand.push(gameDeck.shift());
+
+  //which card just got added 1-7, and then assign the card's array to current card.
+  const currentCard = (player.hand[(player.hand.length - 1)]);
+  const imageSlot = (player.hand.length);
+
+  //assign image to corresponding div.  The first command didn't work, find out why.
+  //document.getElementById (`playerCard ${imageSlot}`).src = `/cardImg/ ${currentCard[0]}.png`;
+  document.getElementById('playerCard' + imageSlot).src = "/cardImg/" + (currentCard[0]) + ".png";
+
   updatePlayerHand();
+
+  //allow hit or double bet.
   if (player.cardTotal >= 21) { document.getElementById("hit").disabled = true };
   if (player.hand.length > 2) { document.getElementById("doubleBet").disabled = true };
 }
 
 function oneToDealer() {
   dealer.hand.push(gameDeck.shift());
-}
+  if (dealer.hand.length != 2) {
+  const currentCard = (dealer.hand[(dealer.hand.length - 1)]);
+  const imageSlot = (dealer.hand.length);
+  document.getElementById('dealerCard' + imageSlot).src = "/cardImg/" + (currentCard[0]) + ".png";
+  } else {
+    document.getElementById('dealerCard2').src = "/cardImg/cardBack.png";
+  };
+};
 
 function doubleBet() {
   player.money -= player.currentBet;
@@ -108,7 +130,6 @@ function doubleBet() {
 
 function updatePlayerHand() {
   player.cardTotal = calcHand(player.hand);
-  document.getElementById("playerHand").innerHTML = "Player's Hand: " + (player.hand);
   document.getElementById("playerTotal").innerHTML = "Player total: " + (player.cardTotal);
   if (player.total >= 21) {
     finishGame();
@@ -120,7 +141,6 @@ function updatePlayerHand() {
 
 function updateDealerHand() {
   dealer.cardTotal = calcHand(dealer.hand);
-  document.getElementById("dealerHand").innerHTML = "Dealer's Hand: " + dealer.hand;
   document.getElementById("dealerTotal").innerHTML = "Dealer total: " + dealer.cardTotal;
 }
 
@@ -134,7 +154,6 @@ function beginGame() {
   document.getElementById("hit").disabled = false;
   document.getElementById("stay").disabled = false;
   updatePlayerHand();
-  document.getElementById("dealerHand").innerHTML = "Dealer's Hand: " + dealer.hand[0] + " ??";
 
   document.getElementById("status").innerHTML = "Decisions Decisions..."
   var hit = document.getElementById("hit");
@@ -164,8 +183,11 @@ function finishGame() {
 }
 
 function dealerTurn() {
+  const currentCard = (dealer.hand[(dealer.hand.length - 1)]);
+  document.getElementById('dealerCard2').src = "/cardImg/" + (currentCard[0]) + ".png";
+
   while ((dealer.cardTotal < 17 && player.cardTotal <= 21) && (dealer.cardTotal <= player.cardTotal)) {
-    dealer.hand.push(gameDeck.shift());
+    oneToDealer();
     updateDealerHand();
   }
 
